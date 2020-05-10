19dc5158d1a4d0e88eb054b96da7f5efdf552a05
d2vts <dev.taeseung@gmail.com>
2020-05-11 03:25:40 +0900

    회원가입폼 수정 & 유효성검사 & CSS와 JS 수정

--- public/javascripts/sign_up.js
+++ public/javascripts/sign_up.js
@@ -12,55 +12,74 @@ $(function () {
                 console.log("ajax에서 받은 값은 : " + data);
                 if ($.trim(data) == '1') {
                     if ($('#id').val() != '') {
-                        
+
+                        var id = $("#id").val();
+                        var regID = /^[a-z0-9]{4,15}$/;
+
+                        if (false === regID.test(id)) {
+                            $("#id_form_guide").css("display", "").css("color", "red").css('font-size', '8px').text('영 소문자, 숫자 4~20자리로 입력해주세요.');
+                            $("#id_form_check_br").css("display", "none");
+                        } else {
+                            $("#id_form_guide").css("display", "").css("color", "#2DB400").css("font-size", "11px").text('사용 가능합니다.');
+                        }
+                    } else {
+                        $("#id_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('필수 정보 입니다.');
+                        $("#id_form_check_br").css("display", "none");
                     }
-                } 
-                else if($.trim(data) == '0'){
-                }
-                
-                else {
-                    if ($('#id').val() != '') {
-                        alert(data);
-                    }
+                } else {
+                    $("#id_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('이미 사용중인 아이디 입니다.');
+                    $("#id_form_check_br").css("display", "none");
                 }
             }
         })
     })
 
 
+    // 시간날때 비밀번호 공백시 alert 울리게 구현 해볼것
+
+
+    // ID 중복값 체크 & 유효성 검사
+    $('#id').blur(function () {
+
+        var id = $("#id").val();
+        var regID = /^[a-z0-9]{4,15}$/;
+
+        if ($('#id').val() != '') {
+            if (false === regID.test(id)) {
+                $("#id_form_guide").css("display", "").css("color", "red").css('font-size', '8px').text('영 소문자, 숫자 4~20자리로 입력해주세요.');
+                $("#id_form_check_br").css("display", "none");
+            } else {
+                $("#id_form_guide").css("display", "").css("color", "#2DB400").css("font-size", "11px").text('사용 가능합니다.');
+                $("#id_form_check_br").css("display", "none");
+
+            }
+        } else {
+            $("#id_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('필수 정보 입니다.');
+            $("#id_form_check_br").css("display", "none");
+        }
+    })
+
+
+
+
 
     $('#pw').blur(function () {
 
         var pw = $("#pw").val();
-            
-        var reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/;
-         
-        if ($('#pw').val() != ''){
-        if(false === reg.test(pw)) {
-        $("#pw_form_error").css("display", "");
-                $("#pw_form_ok").css("display", "none");
+        var regPW = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/;
+
+        if ($('#pw').val() != '') {
+            if (false === regPW.test(pw)) {
+                $("#pw_form_guide").css("display", "").css("color", "red").css('font-size', '8px').text('8~20자 영문대문자, 영문소문자, 숫자, 특수문자를 모두 사용해야합니다.');
                 $("#pw_form_check_br").css("display", "none");
-                $("#pw_form_none").css("display", "none")
-        }else if(/(\w)\1\1\1/.test(pw)){
-         alert('같은 문자를 4번 이상 사용하실 수 없습니다.');
-         return false;
-         }else if(pw.search(/\s/) != -1){
-         alert("비밀번호는 공백 없이 입력해주세요.");
-         return false;
-         }else {
-            $("#pw_form_error").css("display", "none");
+            } else {
+                $("#pw_form_guide").css("display", "").css("color", "#2DB400").css("font-size", "11px").text('사용 가능합니다.');
+                $("#pw_form_check_br").css("display", "none");
+            }
+        } else {
+            $("#pw_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('필수 정보 입니다.');
             $("#pw_form_check_br").css("display", "none");
-            $("#pw_form_ok").css("display", "");
-            $("#pw_form_none").css("display", "none")
-         }
         }
-        else{
-            $("#pw_form_error").css("display", "none");
-            $("#pw_form_check_br").css("display", "none");
-            $("#pw_form_ok").css("display", "none");
-            $("#pw_form_none").css("display", "");
-        }
-    
     })
 
 
@@ -69,33 +88,80 @@ $(function () {
     $('#pw2').blur(function () {
         if ($('#pw').val() != $('#pw2').val()) {
             if ($('#pw2').val() != '') {
-                $("#pw2_form_error").css("display", "");
-                $("#pw2_form_ok").css("display", "none");
+                $("#pw2_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('비밀번호가 일치하지 않습니다.');
                 $("#pw2_form_check_br").css("display", "none");
-                $("#pw2_form_none").css("display", "none");
+            } else {
+                $("#pw2_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('필수정보 입니다.');
+                $("#pw2_form_check_br").css("display", "none");
             }
-            else{
-            $("#pw2_form_error").css("display", "none");
-            $("#pw2_form_check_br").css("display", "none");
-            $("#pw2_form_ok").css("display", "none");
-            $("#pw2_form_none").css("display", "");
-            }
-        }
-        else {
+        } else {
             if ($('#pw2').val() != '') {
-                $("#pw2_form_error").css("display", "none");
+                $("#pw2_form_guide").css("display", "").css("color", "#2DB400").css("font-size", "11px").text("비밀번호 일치");
                 $("#pw2_form_check_br").css("display", "none");
-                $("#pw2_form_ok").css("display", "");
-                $("#pw2_form_none").css("display", "none");
-            }
-            else{
-            $("#pw2_form_error").css("display", "none");
-            $("#pw2_form_check_br").css("display", "none");
-            $("#pw2_form_ok").css("display", "none");
-            $("#pw2_form_none").css("display", "");
+            } else {
+                $("#pw2_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('필수정보 입니다.');
+                $("#pw2_form_check_br").css("display", "none");
             }
         }
     })
 
+    //닉네임 중복체크 와 유효성 검사
+    $('#nickname').blur(function () {
+        $.ajax({
+            type: "POST",
+            url: "/user2/check_nickname",
+            dataType: 'json',
+            data: {
+                "nickname": $('#nickname').val()
+            },
+            success: function (data) {	//data : check_nickname에서 넘겨준 결과값
+                console.log("ajax에서 받은 값은 : " + data);
+                if ($.trim(data) == '1') {
+                    if ($('#nickname').val() != '') {
+
+                        var nickname = $("#nickname").val();
+                        var regNICKNAME = /^[a-zA-Z0-9가-힣]{2,8}$/;
+
+                        if (false === regNICKNAME.test(nickname)) {
+                            $("#nickname_form_guide").css("display", "").css("color", "red").css('font-size', '8px').text('영문,한글,숫자사용가능 2~8단어');
+                            $("#nickname_form_check_br").css("display", "none");
+                        } else {
+                            $("#nickname_form_guide").css("display", "").css("color", "#2DB400").css("font-size", "11px").text('사용 가능합니다.');
+                        }
+                    } else {
+                        $("#nickname_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('필수 정보 입니다.');
+                        $("#nickname_form_check_br").css("display", "none");
+                    }
+                } else {
+                    $("#id_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('이미 사용중인 닉네임 입니다.');
+                    $("#id_form_check_br").css("display", "none");
+                }
+            }
+        })
+    })
+
+
+    
+    $('#phone').blur(function () {
+
+        var phone = $("#phone").val();
+        var regPHONE = /^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
+
+        if ($('#phone').val() != '') {
+            if (false === regPHONE.test(phone)) {
+                $("#phone_form_guide").css("display", "").css("color", "red").css('font-size', '8px').text('전화번호 양식을 벗어납니다.');
+                $("#phone_form_check_br").css("display", "none");
+            } else {
+                $("#phone_form_guide").css("display", "").css("color", "#2DB400").css("font-size", "11px").text('확인');
+                $("#phone_form_check_br").css("display", "none");
+            }
+        } else {
+            $("#phone_form_guide").css("display", "").css("color", "red").css("font-size", "11px").text('필수 정보 입니다.');
+            $("#phone_form_check_br").css("display", "none");
+        }
+    })
+
+
 
 });
+
--- routes/user2.js
+++ routes/user2.js
@@ -3,25 +3,34 @@ var router = express.Router();
 var db = require('../lib/db');
 
 router.post('/check_id', function (req, res, next) {
-
     let form_id = req.body.id;
     //body에 객체형태로 id값이 들어옴 ex){ { id: 'efef' }}
     console.log("회원가입 폼에서 들어온 post 값 : ", form_id);
     
     db.query(`SELECT user_id FROM users`, function(error, user_id_in_db){
         if(error) console.log("[mysql] users DB -> user2.js error ");
-        var result= user_id_in_db[0].user_id;
-
-        if(result!=form_id){
-            res.send('1');
-            console.log("ID CHECK ajax로 전송완료");
-        }
-        else{
-            res.send('0');
-            console.log("ID CHECK ajax로 전송완료");
-        }
+       var result = '1';
+       for(var i = 0 ; i < user_id_in_db.length ; i++){
+        if(user_id_in_db[i].user_id==form_id) result ='0';        
+    }
+    res.send(result);
     });
-
 });
+
+router.post('/check_nickname', function (req, res, next) {
+    let form_nickname = req.body.nickname;
+    //body에 객체형태로 nickname값이 들어옴 ex){ { nickname: 'efef' }}
+    console.log("회원가입 폼에서 들어온 post 값 : ", form_nickname);
+    
+    db.query(`SELECT nickname FROM users`, function(error, nickname_in_db){
+        if(error) console.log("[mysql] users DB -> user2.js error ");
+       var result = '1';
+       for(var i = 0 ; i < nickname_in_db.length ; i++){
+        if(nickname_in_db[i].nickname==form_nickname) result ='0';        
+    }
+    res.send(result);
+    });
+});
+
 
 module.exports = router;
